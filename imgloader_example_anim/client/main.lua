aeb5232c01dee7ab2acd58f4b05a6a6bd3a38d0a
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-12-07 18:48:41 +0100

    Add anim example

---
+++ imgloader_example_anim/client/main.lua
@@ -0,0 +1,59 @@
+--AddEventHandler('imgloader:ready', function()
+
+  Citizen.CreateThread(function()
+
+    RequestStreamedTextureDict('custom_imgs_2', true)
+
+    while not HasStreamedTextureDictLoaded('custom_imgs_2') do
+      Citizen.Wait(0)
+    end
+
+    for k,v in pairs(Config.Animations) do
+      LoadImage('imgloader_example2:' .. k, v.txd, v.name .. '0')
+    end
+
+  end)
+
+--end)
+
+
+for k,v in pairs(Config.Animations) do
+
+  local scope = function(k, v)
+
+    Citizen.CreateThread(function()
+
+      while true do
+
+        for i=v.first, v.last, 1 do
+
+          local camRot  = GetGameplayCamRot()
+
+          SetImage('imgloader_example2:' .. k, {
+            txd    = v.txd,
+            name   = v.name .. i,
+            x      = v.x,
+            y      = v.y,
+            z      = v.z,
+            rotX   = -90.0,
+            rotY   = 0.0,
+            rotZ   = camRot.z,
+            scaleX = v.width,
+            scaleY = v.height,
+            scaleZ = 0.0,
+            alpha  = 100.0,
+          })
+
+          Citizen.Wait(20)
+
+        end
+
+      end
+
+    end)
+
+  end
+
+  scope(k, v)
+
+end
---
+++ imgloader_example_anim/config.lua
@@ -0,0 +1,7 @@
+Config              = {}
+Config.DrawDistance = 50.0
+
+Config.Animations = {
+  anim1 = {x = -6.266, y = 16.576, z = 70.229, size = 3.0, txd = 'custom_imgs_2', name = 'twitch_', first = 0, last = 140,  width = 5.0, height = 2.0},
+  anim2 = {x = -6.266, y = 13.576, z = 70.229, size = 3.0, txd = 'custom_imgs_2', name = 'twitch_', first = 0, last = 140,  width = 5.0, height = 2.0},
+}
