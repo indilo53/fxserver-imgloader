03ff9397046570c46ec631a899d6aa4ad2a092b9
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-12-12 09:11:40 +0100

    Remove comments

--- imgloader/client/wrapper.lua
+++ imgloader/client/wrapper.lua
@@ -1,24 +1,3 @@
---[[function LoadImage(name, txd, imagename)
-
-	local scaleform = nil
-
-  TriggerEvent('imgloader:loadImage', name, txd, imagename, function(_scaleform)
-  	scaleform = _scaleform
-  end)
-
-  return scaleform
-
-end
-
-function SetImage(name, key, val)
-  TriggerEvent('imgloader:setImage', name, key, val)
-end
-
-function UnloadImage(name)
-  TriggerEvent('imgloader:unloadImage', name)
-end
---]]
-
 function CreateImage(params)
 	
 	local params = params or {}
